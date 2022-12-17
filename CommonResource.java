56d0324add33210d5c2b3324d6e90233d15062f6
mcherevik <mcherevik1@gmail.com>
2022-12-17 22:02:24 +0200

    Created generate and print methods in CommonResource

--- CommonResource.java
+++ CommonResource.java
@@ -1,5 +1,26 @@
 package threads1712;
 
+import java.util.Random;
+
 public class CommonResource {
-    String[] letters = {"ABBCGH", "USIROJ ", "AHEUOW "};
+    int num = 3;
+    int stringLength = 6;
+    String[] letters = new String[num];
+    String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    Random random = new Random();
+
+    void generateArray() {
+        for (int i = 0; i < num; i++) {
+            letters[i] = "";
+            for (int j = 0; j < stringLength; j++) {
+                letters[i] += alphabet.charAt(random.nextInt(0, alphabet.length() - 1));
+            }
+        }
+    }
+
+    void printArray() {
+        for (String s : letters) {
+            System.out.print(s + " ");
+        }
+    }
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ package threads1712;
 
 public class Main {
     public static void main(String[] args) {
-
+        CommonResource res = new CommonResource();
+        res.generateArray();
+        res.printArray();
     }
 }
